3054f9fec9534be64764981301015c04df68eb8b
David Alvarez <hola@alvarezdavid.com>
2020-11-03 05:17:01 +0000

    Adding Gorilla framework to Project

--- go_service/go.mod
+++ go_service/go.mod
@@ -4,8 +4,9 @@ go 1.15
 
 require (
 	github.com/go-git/go-billy/v5 v5.0.0
-	github.com/go-git/go-git v4.7.0+incompatible // indirect
+	github.com/go-git/go-git v4.7.0+incompatible
 	github.com/go-git/go-git/v5 v5.1.0
+	github.com/gorilla/mux v1.8.0
 	github.com/imdario/mergo v0.3.11 // indirect
 	github.com/sergi/go-diff v1.1.0 // indirect
 	golang.org/x/crypto v0.0.0-20200930160638-afb6bcd081ae // indirect
--- go_service/modules/examples/commit_example.go
+++ go_service/modules/examples/commit_example.go
@@ -5,8 +5,10 @@ package examples
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
 func main() {
@@ -32,4 +34,26 @@ func main() {
 	fmt.Println("Message of this commit: ", message)
 	fmt.Println("hash of this commit: ", hash)
 	fmt.Println("Time stamp of the commit: ", time)
+}
+
+func ListFilesDirectories(Path string) ([]string, error) {
+	repo, err := git.PlainOpen(Path)
+	var files []string
+	if repo != nil {
+		treeIter, errW := repo.TreeObjects()
+
+		if treeIter != nil {
+			treeIter.ForEach(func(t *object.Tree) error {
+				t.Files().ForEach(func(f *object.File) error {
+					files = append(files, f.Name)
+					return nil
+				})
+				return nil
+			})
+		}
+		if errW != nil {
+			log.Printf("There is not a tree %s", errW)
+		}
+	}
+	return files, err
 }
