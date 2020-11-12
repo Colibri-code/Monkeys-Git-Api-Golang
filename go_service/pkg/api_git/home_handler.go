7dbe3f1c4ed86b400d8be759652686edf51bdffc
David Alvarez <hola@alvarezdavid.com>
2020-11-12 06:49:23 +0000

    Adding json objects in the tree_handler

--- go_service/pkg/api_git/home_handler.go
+++ go_service/pkg/api_git/home_handler.go
@@ -1,15 +1,11 @@
 package api_git
 
 import (
-	"encoding/json"
 	"fmt"
-	"go_service/examples"
 	"net/http"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	directory := "./../"
-	files, _ := examples.ListFilesDirectories(directory)
-	encodeData, _ := json.Marshal(files)
-	fmt.Fprintf(w, string(encodeData))
+
+	fmt.Fprintf(w, "Home")
 }
