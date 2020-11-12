c38fbb145bbf6b19272aebfcad15642233ab1d39
David Alvarez <hola@alvarezdavid.com>
2020-11-12 04:26:30 +0000

    Create a object Request to tag

--- go_service/pkg/api_git/tag_handler.go
+++ go_service/pkg/api_git/tag_handler.go
@@ -4,6 +4,11 @@ import (
 	"net/http"
 )
 
+type TagRequest struct {
+	Url string `json:"url"`
+	Tag string `json:"tag"`
+}
+
 func CopyRepoFromTagHandler(w http.ResponseWriter, r *http.Request) {
 
 }
