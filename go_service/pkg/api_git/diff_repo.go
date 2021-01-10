187c73a9fb1a720adbb6bbfd61e06b70af5b4abb
David Alvarez <hola@alvarezdavid.com>
2021-01-10 05:27:02 +0000

    Improving diff method

--- go_service/pkg/api_git/diff_repo.go
+++ go_service/pkg/api_git/diff_repo.go
@@ -1,6 +1,9 @@
 package api_git
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
@@ -38,5 +41,50 @@ func diffToHead(path string, hash string) (*object.Patch, error) {
 		return nil, err
 	}
 	return patch, err
+
+}
+
+func diffTreeRepos(diffRequest DiffRequest) (object.Changes, error) {
+	if diffRequest.UrlMain != "" && diffRequest.UrlDiff != "" {
+
+		mainRepo, err := git.PlainOpen(diffRequest.UrlMain)
+		diffRepo, err := git.PlainOpen(diffRequest.UrlDiff)
+
+		var refMainHead *plumbing.Reference
+		var refDiffHead *plumbing.Reference
+		var commitMain *object.Commit
+		var commitDiff *object.Commit
+		var objectChanges object.Changes
+
+		if diffRequest.HashMain != "" {
+			commitMain, err = getCommit(diffRequest.HashMain, *mainRepo)
+		} else {
+			refMainHead, err = mainRepo.Head()
+			commitMain, err = mainRepo.CommitObject(refMainHead.Hash())
+		}
+
+		if diffRequest.HashDiff != "" {
+			commitDiff, err = getCommit(diffRequest.HashDiff, *diffRepo)
+		} else {
+			refDiffHead, err = diffRepo.Head()
+			commitDiff, err = diffRepo.CommitObject(refDiffHead.Hash())
+		}
+
+		if err == nil {
+			treeMain, err := commitMain.Tree()
+			treeDiff, err := commitDiff.Tree()
+			if err == nil {
+				objectChanges, err = object.DiffTree(treeMain, treeDiff)
+			}
+		}
+
+		if err != nil {
+			fmt.Print("Error" + err.Error())
+			return nil, err
+		}
+		return objectChanges, nil
+	} else {
+		return nil, errors.New("No contiene la direccion de algunos de los repositorios")
+	}
 
 }
