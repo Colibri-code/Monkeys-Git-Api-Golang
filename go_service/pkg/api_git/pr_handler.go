9434d9800f24d6350cdf648d0c4e3e966b378fe8
antonio <antoniosalas1996@gmail.com>
2021-02-26 01:01:47 +0000

    little changes

--- go_service/pkg/api_git/pr_handler.go
+++ go_service/pkg/api_git/pr_handler.go
@@ -2,6 +2,7 @@ package api_git
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"go_service/tools"
@@ -10,27 +11,31 @@ import (
 	"net/http"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type PRCreate struct {
-	Id               primitive.ObjectID `bson: "_id" `
-	NumberPR         int                `bson:"numberPR,omitempty"`         // Consecutive number of the pull request
-	IdUser           int                `bson:"idUser,omitempty"`           // id user to create a pull request
-	Title            string             `bson:"title,omitempty"`            // Title of thr pull request
-	Body             string             `bson:"body,omitempty"`             // Comment of the pull request
-	UrlRepoReceivePR string             `bson:"urlRepoReceivePR,omitempty"` // Url to repo the receive the pull request
-	UrlRepoCreatePR  string             `bson:"urlRepoCreatePR,omitempty"`  // Url the repo to create a pull request
-	CommitHash       string             `bson:"commitHash,omitempty"`       // hash the commit
-	Patch            string             `bson:"patch,omitempty"`            // Differences
-	BranchNamePR     string             `bson:"branchNamePR,omitempty"`
-	IsLocked         bool               `bson:"isLocked,omitempty"`
-	Mergeable        bool               `bson:"mergeable,omitempty"`
-	HasMerged        bool               `bson:"hasMerged,omitempty"`
-	Merged           time.Time          `bson:"merged,omitempty"`
-	MergedCommitID   string             `bson:"mergedCommitID,omitempty"`
-	MergedBy         int                `bson:"mergedBy,omitempty"`
+	Id               primitive.ObjectID `json:"_id, omitempty" bson: "_id, omitempty" `
+	NumberPR         int                `json:"numberPR, omitempty" bson:"numberPR,omitempty"`                 // Consecutive number of the pull request
+	IdUser           int                `json:"idUser, omitempty" bson:"idUser,omitempty"`                     // id user to create a pull request
+	Title            string             `json:"title, omitempty" bson:"title,omitempty"`                       // Title of thr pull request
+	Body             string             `json:"body, omitempty" bson:"body,omitempty"`                         // Comment of the pull request
+	UrlRepoReceivePR string             `json:"urlRepoReceivePR, omitempty" bson:"urlRepoReceivePR,omitempty"` // Url to repo the receive the pull request
+	UrlRepoCreatePR  string             `json:"urlRepoCreatePR, omitempty" bson:"urlRepoCreatePR,omitempty"`   // Url the repo to create a pull request
+	CommitHash       string             `json:"commitHash, omitempty" bson:"commitHash,omitempty"`             // hash the commit
+	Patch            string             `json:"patch, omitempty" bson:"patch,omitempty"`                       // Differences
+	BranchNamePR     string             `json:"branchNamePR, omitempty" bson:"branchNamePR,omitempty"`
+	IsLocked         bool               `json:"isLocked, omitempty" bson:"isLocked,omitempty"`
+	Mergeable        bool               `json:"mergeable, omitempty" bson:"mergeable,omitempty"`
+	HasMerged        bool               `json:"hasMerged, omitempty" bson:"hasMerged,omitempty"`
+	Merged           time.Time          `json:"merged, omitempty" bson:"merged,omitempty"`
+	MergedCommitID   string             `json:"mergedCommitID, omitempty" bson:"mergedCommitID,omitempty"`
+	MergedBy         int                `json:"mergedBy, omitempty" bson:"mergedBy,omitempty"`
 }
+
+var client *mongo.Client
 
 func PRHandler(w http.ResponseWriter, r *http.Request) {
 	var prCreate PRCreate
@@ -77,6 +82,50 @@ func PRHandler(w http.ResponseWriter, r *http.Request) {
 
 func InsertOne(w http.ResponseWriter, r *http.Request) {
 
+	w.Header().Add("content-type", "application/json")
+
 	tools.ConnectionDB()
 
+	var Pr PRCreate
+
+	_ = json.NewDecoder(r.Body).Decode(&Pr)
+
+	collection := client.Database("go_git").Collection("PR_collection")
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	result, _ := collection.InsertOne(ctx, Pr)
+
+	json.NewEncoder(w).Encode(result)
+
+}
+
+func GetOne(res http.ResponseWriter, req *http.Request) {
+
+	res.Header().Add("content-type", "application/json")
+
+	tools.ConnectionDB()
+
+	var PR []PRCreate
+
+	database := client.Database("go_git")
+	PRcollection := database.Collection("PR_collection")
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	cursor, err := PRcollection.Find(ctx, bson.M{})
+
+	if err != nil {
+
+		res.Write([]byte(` {"message":" ` + err.Error() + `"}`))
+		return
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var PResquest PRCreate
+		cursor.Decode(&PResquest)
+		PR = append(PR, PResquest)
+	}
+
+	json.NewEncoder(res).Encode(PR)
 }
--- go_service/tools/dbconnection.go
+++ go_service/tools/dbconnection.go
@@ -1,33 +1,31 @@
 package tools
 
 import (
+	"context"
 	"fmt"
 	"log"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-	"context"
 	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+func ConnectionDB() {
 
-func ConnectionDB()  {
-	
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://mongo:27017"))
-	
+
 	if err != nil {
-    log.Fatal(err)
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
 }
-
-// Check the connection
-err = client.Ping(context.TODO(), nil)
-
-if err != nil {
-    log.Fatal(err)
-}
-
-fmt.Println("Connected to MongoDB!")
-}
-
-
